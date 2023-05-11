bb4856cb7355e4c106273d73541f5cbf482b8997
romanssam <troubledyouthh@outlook.com>
2023-05-11 14:28:06 +0500

    1
    1

--- server.mjs
+++ server.mjs
@@ -18,7 +18,7 @@ const privateVapidKey = process.env.VAPID_PRIVATE_KEY
 
 console.log({publicVapidKey, privateVapidKey})
 
-let subscriptionData;
+let subscriptionData = null;
 
 webpush.setVapidDetails(
   `mailto:${process.env.VAPID_MAILTO}`,
