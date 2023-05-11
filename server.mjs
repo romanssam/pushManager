bc55971fbb24411ebc31a4e744f1ed503601951f
romanssam <troubledyouthh@outlook.com>
2023-05-11 14:45:34 +0500

    1
    1

--- server.mjs
+++ server.mjs
@@ -1,11 +1,15 @@
 import express from "express";
 import webpush from "web-push";
-import path from "path";
+import { fileURLToPath } from 'url';
+import path from 'path';
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors"
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 app.use(express.json())
